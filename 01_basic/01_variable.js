a501739ad18f9052ff3decefb2a8611a09d5b86f
kumarstl <souravkr.stl@gmail.com>
2025-06-21 19:05:22 +0000

    container 1 done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountId =14554
+let accountEmail ="sourav@123gmail.com"
+var accountPassword ="1235"
+ accountCity="Bihar"
+
+// accountId=2   // not allowed
+accountEmail="abc@.com"
+accountPassword="124478"
+accountCity="Delhi"
+ let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and funcation scope.
+*/
+
