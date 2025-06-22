c72f1c26d134deaf2752e07e6e0870ea53699228
kumarstl <souravkr.stl@gmail.com>
2025-06-22 11:22:10 +0000

    DataType

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,29 @@
+"use strict";  // Treat all JS code as newer version.
+//alert (3+5) // we are using node js; not browser.
+
+console.log(3
+    
+    +9);  // code readbality should be high
+
+console.log("sourav")
+
+let name="soura";
+let age =  20
+let isLoggedIn = true
+
+///premetative data type
+// number => 2 to power 53
+//bigint
+//string=>""
+//Boolean=>True/False
+//null=> standalone value
+//undefined=>
+    //symbol=>unique
+
+
+//Object
+console.log(typeof undefined);  //undefined
+console.log(typeof null);    //Object
+
+
+
