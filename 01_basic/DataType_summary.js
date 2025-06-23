00e83c74ddffad19220506df978e4dc7e1b46381
Sourav Kumar <souravkr.stl@gmail.com>
2025-06-23 19:09:09 +0000

    DataType_summary

---
+++ 01_basic/DataType_summary.js
@@ -0,0 +1,31 @@
+//#Primitive
+// 7 Types:string; Number; Boolean; Null; Undefined; Sybol;BigInt
+//Integer datatype
+const score =100
+const scorevalue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id= Symbol('123')
+const anotherId=('123')
+
+console.log(id===anotherId);
+
+//const bigNumber=4874454664645351n
+
+// #Reference: (Non primitive)
+// Array,Objects,Funcation
+
+const heros =["shaktiman","naagraj","doga"]
+
+let myobj={
+    name:"sourav",
+    age:22,
+}
+
+const myfuncation=function(){
+    console.log("Hello sourav");   
+}
+ console.log( typeof outsideTemp);
