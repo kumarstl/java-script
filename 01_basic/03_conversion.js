bb43c75646229e6b0a364d8eaf43cbec01d058d4
Sourav Kumar <souravkr.stl@gmail.com>
2025-06-23 09:31:24 +0000

    conversion of DataType

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -21,6 +21,61 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 //""=>false
 //"hitesh"=>true
 
-let someNumber= 33
-let stringNumber=string(someNumber)
-console.log(typeofstringNumber);
+// let someNumber= 33
+// let stringNumber=string(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**************Operation*****************
+
+let value = 3
+let negvalue=-value
+//console.log(negvalue);
+
+// console.log(5+5);
+// console.log(6-4);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/5);
+// console.log(2%5);
+
+let str1 ="hello"
+let str2 =" sourav"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+4*5%4);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1=num2,num3=2+5
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
