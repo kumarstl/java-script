225fa2de8de0547a4639b2066957f4af12bb273a
Sourav Kumar <souravkr.stl@gmail.com>
2025-06-23 06:57:41 +0000

    Conversion of DataTpye

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,26 @@
+let score = "35"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"35"=> 33
+// "35abc"=> NaN -- Not a Number
+// true=> 1; False=>0;
+
+let isLoggedIn =1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+//""=>false
+//"hitesh"=>true
+
+let someNumber= 33
+let stringNumber=string(someNumber)
+console.log(typeofstringNumber);
