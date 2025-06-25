49141d9f1e1d799166433a963cf4406c3b274ddf
Sourav Kumar <souravkr.stl@gmail.com>
2025-06-25 18:33:40 +0000

    Strings....

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,56 @@
+const name ="sourav"
+const repoCount=50
+
+//console.log((name+repoCount+"value"));
+
+//console.log(`Hello My name is ${name} and my repo count is ${repoCount}`);
+
+const gamename = new string('sourav-kr')
+// sconsole.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.touppercase());
+
+console.log(gamename.charAt('2'));
+console.log(gamename.indexof('t'));
+
+const newstring=gamename.substring(0,4)
+console.log(newstring);
+
+
+const anotherstring=gamename.slice(-8,4)
+console.log(anotherstring);
+
+const newstringone =" sourav "
+console.log(newstring)
+console.log(newstringone.trim());
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sundar'));
+
+
+console.log(gamename.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
