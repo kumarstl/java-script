5104c933cd421d6ad1e1e00688229e1300f71631
Sourav Kumar <souravkr.stl@gmail.com>
2025-06-30 10:16:34 +0000

    Funcations..

--- 03_Basic/01_Funcations.js
+++ 03_Basic/01_Funcations.js
@@ -1,11 +1,11 @@
 
 function myName(){
-console.log("s");
-console.log("o");
-console.log("u");
-console.log("r");
-console.log("a");
-console.log("v");
+// console.log("s");
+// console.log("o");
+// console.log("u");
+// console.log("r");
+// console.log("a");
+// console.log("v");
 
 }
 //myName()
@@ -38,5 +38,33 @@ function loginUserMessage(username="sourav"){
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("sourav"));
-console.log(loginUserMessage("kumar sourav"));
+//console.log(loginUserMessage("kumar sourav"));
  
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200,400,500,2000));
+
+const user={
+username:"sourav",
+price:199
+}
+
+function handleObject(anyobject){
+console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+//handleObject(user)
+handleObject({
+    username:" kumar",
+    price:499
+})
+const myNewArray =[200,400,100,600]
+
+function returnSecondvlaue(getArray){
+return getArray[1]
+}
+//console.log(returnSecondvlaue(myNewArray));
+console.log(returnSecondvlaue([200,400,500,2000]));
