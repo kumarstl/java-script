054c1c231f5c172b561f0e113132fd58eed9e9b1
Sourav Kumar <souravkr.stl@gmail.com>
2025-06-30 20:20:43 +0000

    Arrow_Funcations

---
+++ 03_Basic/03_Arrow.js
@@ -0,0 +1,61 @@
+const user ={
+    username:"sourav",
+    price:999,
+
+    WelcomeMessage:function(){
+        console.log(`${this.username},Welcom to Website`);
+        console.log(this);
+        
+    }
+}
+
+// user.WelcomeMessage()
+// user.username="kumar"
+// user.WelcomeMessage()
+
+//console.log(this);
+
+// function chai (){
+//     let username="sourav"
+//     console.log(this.username);
+    
+// }
+
+// chai()
+
+
+
+// const chai = function(){
+//     let username ="sourav"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+const chai = () =>{
+    let username ="sourav"
+   // console.log(this.username);
+    
+}
+//chai()
+
+
+  
+//  const addtwo = (num1,num2) =>{
+//     return num1+num2
+//  }
+//  console.log(addtwo(2,4));
+ 
+
+// const addtwo = (num1,num2) => num1+num2
+
+// const addtwo = (num1,num2) => (num1+num2)
+
+const addtwo = (num1,num2) =>({username:"sourav"})
+
+ 
+console.log(addtwo(2,4));
+ 
+
+// const Myarray=[5,6,8,2]
+// Myarray.forEach()
