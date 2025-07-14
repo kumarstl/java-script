d83954cb0571fae298bdd8c9c70e627a15b48389
Sourav Kumar <souravkr.stl@gmail.com>
2025-07-14 06:02:44 +0000

    Do_While Loops

---
+++ 05_Loops-Iteration/Two.js
@@ -0,0 +1,25 @@
+//*while loop....
+
+let index = 0
+while (index<=10) {
+//console.log(`vlaue of index ${index}`);
+    index=index+2
+}
+
+
+let myarrray =["superman","batmam","flash"]
+
+let arr =0
+while (arr<myarrray.length) {
+   /// console.log(`value is ${myarrray[arr]}`);
+    arr=arr+1
+}
+
+
+
+let score=1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
