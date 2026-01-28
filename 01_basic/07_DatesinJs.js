2f33cfcbfbf74b7586e93c5c349d43206f403908
Sourav Kumar <souravkr.stl@gmail.com>
2026-01-28 20:06:38 +0000

    Date in JS

---
+++ 01_basic/07_DatesinJs.js
@@ -0,0 +1,52 @@
+//Dates..
+
+let myDate=new Date ()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+
+// console.log(typeof myDate); //Output-Object
+
+// let myCreatedDate=new Date(2026,0,29)
+// let myCreatedDate=new Date(2026,0,23,5,3)
+// let myCreatedDate=new Date("2026-01-25")
+
+  let myCreatedDate=new Date("01-14-2026")
+// console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp= Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+
+console.log(newDate);
+console.log(newDate.getFullYear());
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
+
+// `${newDate.getMonth} and the time`
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+ 
