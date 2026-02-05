c2d815bcf2ea12f4b6f52a3f8dd09f787f0c2969
Sourav Kumar <souravkr.stl@gmail.com>
2026-02-05 18:11:40 +0000

    getter_setters

---
+++ 11_Objects../getter_setters.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const sourav = new User("@sourav.ai", "abc")
+console.log(sourav.email);
