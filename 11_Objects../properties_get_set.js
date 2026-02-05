758ea8a7d4374031e564cbfdd5d943b996dd8160
Sourav Kumar <souravkr.stl@gmail.com>
2026-02-05 18:19:51 +0000

    properties_get

--- 11_Objects../properties_get_set.js
+++ 11_Objects../properties_get_set.js
@@ -1,13 +1,27 @@
-function User(email,password){
-    this._email=email;
-    this._password=password;
 
-    Object.defineProperties(this,'email',{
-        get:function(){
-            return this._email.toUppercase()
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
         },
-        set:function(value){
-            this.email=value
+        set: function(value){
+            this._email = value
         }
     })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
 }
+
+const sourav = new User("@sourav.com", "kumar")
+
+console.log(sourav.email);
