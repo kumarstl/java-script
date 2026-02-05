49b1341249e12b9f4816381ecae1357f03eb4057
Sourav Kumar <souravkr.stl@gmail.com>
2026-02-05 18:23:10 +0000

    object_get

---
+++ 11_Objects../object_get.js
@@ -0,0 +1,16 @@
+const User = {
+    _email: 'sour@a.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
